9f81748526be0ef85becbaa10cff5363a8a6447e
Daniel Conlon <danielcconlon@gmail.com>
2022-11-23 13:48:15 -0500

    initial spring boot files

---
+++ src/main/java/com/warzone2/Warzone2GunApiApplication.java
@@ -0,0 +1,13 @@
+package com.warzone2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Warzone2GunApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Warzone2GunApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/warzone2/Warzone2GunApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warzone2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Warzone2GunApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

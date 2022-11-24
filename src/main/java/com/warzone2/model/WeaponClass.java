16237925dc19f9b340556650a655a6a9b4c25bac
Daniel Conlon <danielcconlon@gmail.com>
2022-11-24 12:54:11 -0500

    created weaponclass model structure

---
+++ src/main/java/com/warzone2/model/WeaponClass.java
@@ -0,0 +1,12 @@
+package com.warzone2.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "weaponClass")
+public class WeaponClass {
+}

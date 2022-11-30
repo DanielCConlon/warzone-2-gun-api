2fe508a2325721c484cdb77dc98089bbf742f3dd
Daniel Conlon <danielcconlon@gmail.com>
2022-11-30 15:59:51 -0500

    finished the UserControllers

--- src/main/java/com/warzone2/controller/UserController.java
+++ src/main/java/com/warzone2/controller/UserController.java
@@ -5,9 +5,9 @@ import com.warzone2.model.WeaponClass;
 import com.warzone2.repository.UserRepository;
 import com.warzone2.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -43,6 +43,30 @@ public class UserController {
         }
 
         return returnUser;
+    }
+
+    @PostMapping("/api/users")
+    public User addUser(@RequestBody User user) {
+//        encrypt password
+        user.setPassword(BCrypt.hashpw(user.getPassword(),BCrypt.gensalt()));
+        repository.save(user);
+        return user;
+    }
+
+
+    @PutMapping("/api/users/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User user) {
+        User tempUser = repository.getById(id);
+        if (!tempUser.equals(null)) {
+            user.setId(tempUser.getId());
+            repository.save(user);
+        }
+        return user;
+    }
+    @DeleteMapping("/api/users/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable int id) {
+        repository.deleteById(id);
     }
 
 }
